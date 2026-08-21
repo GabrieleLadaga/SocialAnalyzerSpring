4dea92c7de5704ea45f1fedd2c1da0113f8679eb
gabrieleladaga <gabrieleladaga@gmail.com>
2026-08-21 17:47:12 +0200

    Initial commit

---
+++ src/main/java/com/unical/sdcc/SocialAnalyzerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unical.sdcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAnalyzerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialAnalyzerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unical/sdcc/SocialAnalyzerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unical.sdcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialAnalyzerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
